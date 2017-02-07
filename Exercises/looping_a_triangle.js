dac8881c65e5638d952657812d141ae5766d7a0a
Tom Hockett <tom.hockett@gmail.com>
2017-02-06 18:32:56 -0600

    Add problem descriptions

--- Exercises/looping_a_triangle.js
+++ Exercises/looping_a_triangle.js
@@ -1,6 +1,19 @@
 /*
   Solution for "Looping a triangle" from Eloquent JavaScript
   by Tom Hockett
+
+  "Write a loop that makes seven calls to console.log to output
+  the following triangle:
+    #
+    ##
+    ###
+    ####
+    #####
+    ######
+    #######
+
+  It ay be useful to know that you can find the length of a string
+  by writing .length after it."
 */
 
 var char = '#';
